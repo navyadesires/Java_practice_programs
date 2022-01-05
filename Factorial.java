631bf648404e3279a1fc2606e094f1a3da644367
Ramya Tripurana <navyadesires@gmail.com>
2022-01-05 19:16:32 +0530

    Factorial program comitted

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+ // find factorial
+class Factorial
+{
+public static void main(string args[])
+{
+int fact = 1,n;
+Scanner in = new Scanner(System.in);
+System.out.println("Enter a n value");
+n = s.nextInt();
+while(n>0)
+{
+fact = fact*n;
+n--;
+}
+System.Out.Println(" the factorial of number is"+fact);
+}
+}
+
+
