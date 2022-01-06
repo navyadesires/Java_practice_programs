463101f6878b4e5879791c91fd74b7d7460be9c8
Ramya Tripurana <navyadesires@gmail.com>
2022-01-06 19:31:01 +0530

    practice example program committed

---
+++ inheritence/MainMethod.java
@@ -0,0 +1,11 @@
+package inheritenceandpolymorphism;
+
+public class Mainmethod {
+public static void main(String[] args) {
+   Rectangle r = new Rectangle();
+   r.circle();
+   r.shape();
+   r.rectangle();
+ 
+}
+}
